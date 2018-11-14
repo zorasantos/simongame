2149b0e8e10367f98c9789c47c548a7f73cdec51
Zora Santos <zoranildo22@gmail.com>
2018-11-14 12:58:24 -0200

    Arquivo Completo

--- simon.js
+++ simon.js
@@ -121,8 +121,114 @@ function four() {
 }
 
 function clearColor() {
-    superiorEsquerda.style.backgroundColor = "darkcolor";
+    superiorEsquerda.style.backgroundColor = "darkgreen";
     superiorDireita.style.backgroundColor = "darkred";
     inferiorEsquerdo.style.backgroundColor = "goldenrod";
     inferiorDireito.style.backgroundColor = "darkblue";
+}
+
+function flashColor() {
+    superiorEsquerda.style.backgroundColor = "lightgreen";
+    superiorDireita.style.backgroundColor = "tomato";
+    inferiorEsquerdo.style.backgroundColor = "yellow";
+    inferiorDireito.style.backgroundColor = "lightskyblue";
+}
+
+superiorEsquerda.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(1);
+        check();
+        one();
+        if(!win) {
+            setTimeout (() => {
+                clearColor();
+            }, 300);
+        }
+    }
+})
+
+superiorDireita.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(2);
+        check();
+        two();
+        if(!win) {
+            setTimeout (() => {
+                clearColor();
+            }, 300);
+        }
+    }
+})
+
+inferiorEsquerdo.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(3);
+        check();
+        three();
+        if(!win) {
+            setTimeout (() => {
+                clearColor();
+            }, 300);
+        }
+    }
+})
+
+inferiorDireito.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(4);
+        check();
+        four();
+        if(!win) {
+            setTimeout (() => {
+                clearColor();
+            }, 300);
+        }
+    }
+})
+
+function check() {
+    if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1])
+        good = false;
+
+    if (playerOrder.length == 3 && good) {
+        winGame();
+    }
+    
+    if (good == false) {
+        flashColor();
+        turnCounter.innerHTML = "NO!";
+        setTimeout(() => {
+            turnCounter.innerHTML = turn;
+            clearColor();
+
+            if (strict) {
+                play();
+            } else {
+                compTurn = true;
+                flash = 0;
+                playerOrder = [];
+                good = true;
+                intervalId = setInterval(gameTurn, 800);
+            }
+        }, 800);
+
+        noise = false;
+    }
+
+    if (turn == playerOrder.length && good && !win) {
+        turn++;
+        playerOrder = [];
+        compTurn = true;
+        flash = 0;
+        turnCounter.innerHTML = turn;
+        intervalId =setInterval(gameTurn, 800);
+    }
+
+}
+
+function winGame() {
+    flashColor();
+    turnCounter.innerHTML = "WIN!";
+    on = false;
+    win = true;
 }
